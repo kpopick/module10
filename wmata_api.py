930711c7279414d3bc69a0fce01ed7ff4f459aed
kpopick <kpopick1@jh.edu>
2025-07-24 21:48:19 -1000

    Instructions up to step 7

---
+++ wmata_api.py
@@ -0,0 +1,46 @@
+import json
+import requests
+from flask import Flask
+
+# API endpoint URL's and access keys
+WMATA_API_KEY = "cb0b71aeb07445e09469b9d09d74e4e6"
+INCIDENTS_URL = "https://api.wmata.com/Incidents.svc/json/ElevatorIncidents"
+headers = {"api_key": WMATA_API_KEY, 'Accept': '*/*'}
+
+################################################################################
+
+app = Flask(__name__)
+
+# get incidents by machine type (elevators/escalators)
+# field is called "unit_type" in WMATA API response
+@app.route("/incidents/<unit_type>", methods=["GET"])
+def get_incidents(unit_type):
+  # create an empty list called 'incidents'
+  incidents = []
+
+  # use 'requests' to do a GET request to the WMATA Incidents API
+  response = requests.get(INCIDENTS_URL, headers = headers)
+  
+  # retrieve the JSON from the response
+  json_response = response.json()
+
+  # iterate through the JSON response and retrieve all incidents matching 'unit_type'
+  for incident in json_response['ElevatorIncidents']:
+    if incident['UnitType'].lower() == unit_type.lower():
+  # for each incident, create a dictionary containing the 4 fields from the Module 7 API definition
+  #   -StationCode, StationName, UnitType, UnitName
+      incident_dict = {
+        'StationCode': incident['StationCode'],
+        'StationName': incident['StationName'],
+        'UnitType': incident['UnitType'],
+        'UnitName': incident['UnitName']
+        }
+  
+  # add each incident dictionary object to the 'incidents' list
+      incidents.append(incident_dict)
+      
+  # return the list of incident dictionaries using json.dumps()
+  return json.dumps(incidents)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
